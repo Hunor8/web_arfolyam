580a925d6c354d09040b142e23d096bfbf323fef
Kovács Hunor <kovacshunorkrisztian@gmail.com>
2025-05-19 12:54:14 +0200

    pénzek

---
+++ alap.js
@@ -0,0 +1,27 @@
+fetch ("https://api.exchangerate-api.com/v4/latest/usd")
+.then (x => x.json())
+.then (y => megjelenitUSD(y))
+
+function megjelenitUSD(y){
+    console.log(y)
+    document.getElementById("usd").innerHTML=y.rates.HUF+" Ft"
+}
+
+fetch ("https://api.exchangerate-api.com/v4/latest/eur")
+.then (x => x.json())
+.then (y => megjelenitEUR(y))
+
+function megjelenitEUR(y){
+    console.log(y)
+    document.getElementById("eur").innerHTML=y.rates.HUF+" Ft"
+}
+
+fetch ("https://api.exchangerate-api.com/v4/latest/gbp")
+.then (x => x.json())
+.then (y => megjelenitGBP(y))
+
+function megjelenitGBP(y){
+    console.log(y)
+    document.getElementById("gbp").innerHTML=y.rates.HUF+" Ft"
+}
+
